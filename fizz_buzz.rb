0545f89665c4afbeddd5868af38cd9ac117f2070
aecarrascosa <ezecarrascosa@gmail.com>
2014-04-22 23:06:45 +0200

    Kata Agregada

---
+++ fizz_buzz.rb
@@ -0,0 +1,9 @@
+class FizzBuzz
+def say(n)
+if (n % 5 == 0)
+return "Buzz"
+else
+return "Fizz"
+end
+end
+end
---
+++ fizz_buzz_test.rb
@@ -0,0 +1,10 @@
+require 'test/unit'
+require_relative 'fizz_buzz'
+
+#fizz_buzz_test.rb
+class FizzBuzzTest < Test::Unit::TestCase
+
+  def fizz_buzz
+    FizzBuzz.new
+  end
+
